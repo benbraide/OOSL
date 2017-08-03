6bc94d78a0084e8fe497e85a00639a99550c018a
benbraide <benbraide@yahoo.com>
2017-08-03 10:48:18 -0600

    Initial

---
+++ oosl/memory/memory_block.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef OOSL_MEMORY_BLOCK_H
+#define OOSL_MEMORY_BLOCK_H
+
+#include <cstdlib>
+
+namespace oosl{
+	namespace memory{
+		
+	}
+}
+
+#endif /* !OOSL_MEMORY_BLOCK_H */
